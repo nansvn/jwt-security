07dc2beacfc1c7d248352e0d6c96569480a60c91
Nan <nansun768@gmail.com>
2023-09-12 14:52:24 +0100

    continue

--- src/main/java/com/springboot/securityproject/config/JwtAuthenticationFilter.java
+++ src/main/java/com/springboot/securityproject/config/JwtAuthenticationFilter.java
@@ -6,15 +6,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+// validation handling class
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailService;
 
     @Override
     protected void doFilterInternal(
@@ -23,16 +30,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Token Authorization");
-        final String jwt;
-        final String userEmail;
-        // If token is null or not valid token, then return bad-request response
+
+        // if the token is invalid or null, return bad-request response
         if (authHeader == null || !authHeader.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
         }
-        // Get the rest of the token except for 'Bearer' header
+
+        // get the rest of the token
+        final String jwt;
         jwt = authHeader.substring(7);
-        // Extract userEmail
-        userEmail = jwtService.extractUsername(jwt);
+        // check if user's email is valid and authorized
+        final String userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // retrieve user info from the database
+            UserDetails userDetails = this.userDetailService.loadUserByUsername(userEmail);
+            // if the token is still valid
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                // create authorized token with user details
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // update the security context holder
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
